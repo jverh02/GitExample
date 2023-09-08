c025d2216f5af06ba73d4139c2fb05d60d9f78e0
jverh02 <63627844+jverh02@users.noreply.github.com>
2023-09-07 19:41:10 -0500

    Update Program.cs
    added line 10

--- TestProj/Program.cs
+++ TestProj/Program.cs
@@ -7,6 +7,7 @@ namespace TestProj
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Github Code Editor!");
         }
     }
 }
